f7d1031a35f4609de4533f34d015af422cb80898
Matias Marquez <matiastmarquez@gmail.com>
2021-06-13 14:25:10 -0300

    Cambios en uno de los tests

--- tests/test_tablero_vacio.py
+++ tests/test_tablero_vacio.py
@@ -9,3 +9,8 @@ def test_tablero_vacio_tiene_7_columnas():
 	tablero = tableroVacio()
 
 	assert len(tablero[0]) == 7
+
+def test_tablero_vacio_es_vacio():
+	tablero = tableroVacio()
+
+	assert tablero == [[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]]
